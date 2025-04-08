0202ff2a796a492b02377dca1aeec3787b0b5973
leduc432003 <anhducle4433@gmail.com>
2025-04-08 15:06:49 +0700

    commit 04/08/2025

--- trading-service/src/main/java/com/duc/trading_service/service/WebSocketConfig.java
+++ trading-service/src/main/java/com/duc/trading_service/service/WebSocketConfig.java
@@ -1,7 +1,5 @@
-package com.duc.trading_service.config;
+package com.duc.trading_service.service;
 
-import com.duc.trading_service.service.CoinPriceWebSocketClient;
-import com.duc.trading_service.service.CoinService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaTemplate;
